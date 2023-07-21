02cfd9fdd39c75d597828f49365f9d925c8dde4a
RAHUL SINGH <125339477+roythakur@users.noreply.github.com>
2023-07-21 21:57:42 +0530

    Create supply

--- ERC-1155.sol
+++ ERC-1155.sol
@@ -5,19 +5,25 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HelloToken is ERC1155, Ownable {
+uint256[] supplies  =[20, 40, 42];
+
     constructor() ERC1155("") {}
 
     function mint( uint256 id, uint256 amount, bytes memory data)
         public
-        onlyOwner
+     
     {
+ require(id<supplies.length, "Token does not exist");
+         require(id>0, "Token does not exist");
+
         _mint(msg.sender, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mintBatch(to, ids, amounts, data);
-    }
+  // for mint batch of tokens
+    // function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    //     public
+    //     onlyOwner
+    // {
+    //     _mintBatch(to, ids, amounts, data);
+    // }
 }
